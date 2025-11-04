e536711b4f7671e2125d56c6c611fdd6bf5236d4
Tamanna-salma <tamannaummey26@gmail.com>
2025-11-04 15:07:20 +0600

    bids function added

--- index.js
+++ index.js
@@ -24,11 +24,20 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
 const db=client.db('smart_db');
 const productsCollection=db.collection('products');
+const bidsCollection=db.collection('Bids');
 
-app.get('/products',async(req,res)=>{
-    const cursor= productsCollection.find();
+app.get('/products',async(req,res)=>{ 
+    // const projectsfields={title:1,image:1,price_min:1,price_max:1}
+    // const cursor= productsCollection.find().sort({price_min:-1}).skip(2).limit(3).project(projectsfields); 
+     const email=req.query.email;
+    const query={}
+    if(email){
+        query.email=email;
+    }
+    const cursor=productsCollection.find(query)
     const result= await cursor.toArray();
     res.send(result)
 });
@@ -41,7 +50,6 @@ app.get('/products/:id',async(req,res)=>{
     const result= await productsCollection.findOne(query)
     res.send(result)
 })
-
 
 app.post('/products',async(req,res)=>{
     const newproduct=req.body;
@@ -69,6 +77,58 @@ app.delete('/products/:id',async(req,res)=>{
    const query={_id:new ObjectId(id)}
    const result=await productsCollection.deleteOne(query);
    res.send(result)
+})
+
+// bids related Apis
+app.get('/Bids',async(req,res)=>{ 
+    const email=req.query.email;
+    const query={};
+    if(email){
+        query.buyer_email=email;   
+    }
+    const cursor=bidsCollection.find(query);
+    const result= await cursor.toArray();
+    res.send(result)
+})
+
+//  get a single bids data
+
+app.get('/Bids/:id',async(req,res)=>{
+    const id=req.params.id;
+    const query= {_id: new ObjectId(id)}
+    const result= await bidsCollection.findOne(query)
+    res.send(result)
+})
+
+ app.post('/Bids',async(req,res)=>{
+    const newbid=req.body;
+    const result= await bidsCollection.insertOne(newbid);
+    res.send(result)
+ })
+
+ app.delete('/Bids/:id',async(req,res)=>{
+   const id=req.params.id; 
+   const query={_id:new ObjectId(id)}
+   const result=await bidsCollection.deleteOne(query);
+   res.send(result)
+})
+
+// Edit a bids
+app.patch('/Bids/:id',async(req,res)=>{
+    const id=req.params.id;
+    const updatedbids=req.body ;
+    const query={_id: new ObjectId(id)}
+    const update={
+        $set:{
+            buyer_name: updatedbids. buyer_name,
+          bid_price: updatedbids. bid_price,
+          buyer_email:updatedbids.buyer_email
+         
+        }
+    }
+    const result=await bidsCollection .updateOne(query,update)
+    res.send(result)
+
 })
 
     await client.db("admin").command({ ping: 1 });
